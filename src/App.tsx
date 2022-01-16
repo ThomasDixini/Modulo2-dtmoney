0f4f3ccedf141e5d255cd6ad3f604bc9d3dfa1da
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-01-16 00:13:25 -0300

    Criando o Resumo da nossa Aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import styled from 'styled-components';
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header/index';
 import { GlobalStyle } from './styles/global'
 
@@ -11,8 +12,9 @@ import { GlobalStyle } from './styles/global'
 export function App() {
   return (
     <>
-      <GlobalStyle/>
       <Header/>
+      <Dashboard/>
+      <GlobalStyle/>
     </>
   );
 }
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from './styles';
+import incomesImg from '../../assets/Entradas.svg'
+import outcomesImg from '../../assets/Saídas.svg'
+import totalImg from '../../assets/Total.svg'
+
+export function Summary() {
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomesImg} alt="incomes" />
+                    
+                </header>
+                <strong>R$5000,00</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={outcomesImg} alt="incomes" />
+                    
+                </header>
+                <strong>-R$1000,00</strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Entradas</p>
+                    <img src={totalImg} alt="incomes" />
+                    
+                </header>
+                <strong>R$4000,00</strong>
+            </div>
+        </Container>
+    );
+}
